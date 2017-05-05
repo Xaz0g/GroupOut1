a5f34ff1efedf72041749d3c66a3d4e02b923277
marietopphem <marietopphem@gmail.com>
2017-05-05 14:26:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marietopphem/groupout1/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.marietopphem.groupout1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.login);
+        loginButton = (LoginButton)findViewById(R.id.fb_login_button);
+        textView =(TextView)findViewById(R.id.textView);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                textView.setText("Login success \n" +
+                        loginResult.getAccessToken().getUserId()+
+                        "\n" + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+                textView.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    public void onButtonClick(View v){
+        if(v.getId()==R.id.logIn ){
+            Intent i = new Intent(MainActivity.this, Home.class);
+            startActivity(i);
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/marietopphem/groupout1/Register.java
@@ -0,0 +1,8 @@
+package com.example.marietopphem.groupout1;
+
+/**
+ * Created by marietopphem on 2017-05-05.
+ */
+
+public class Register {
+}
