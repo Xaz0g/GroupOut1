621bc05348bd11fc621ff04d80d18a4918f6fadb
Malin <malin@aldrin.se>
2017-05-09 13:12:25 +0200

    create spinner

--- app/src/main/java/com/example/marietopphem/groupout1/Create.java
+++ app/src/main/java/com/example/marietopphem/groupout1/Create.java
@@ -6,40 +6,27 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Create extends AppCompatActivity {
-
-    private Spinner spinner;
-    private static final String[]paths = {"Löpning", "item 2", "item 3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
-        spinner = (Spinner)findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Create.this,
-                android.R.layout.simple_spinner_item,paths);
+        Spinner p = (Spinner) findViewById(R.id.spinner);
+        p.setPrompt("Plats");
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        p.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                Toast.makeText(Create.this, parent.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+
+            }
+        });
     }
-
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-
-        switch (position) {
-            case 0:
-                // Whatever you want to happen when the first item gets selected
-                break;
-            case 1:
-                // Whatever you want to happen when the second item gets selected
-                break;
-            case 2:
-                // Whatever you want to happen when the thrid item gets selected
-                break;
-
-        }
-    }
-
 }
