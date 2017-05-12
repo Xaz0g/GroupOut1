c5306e266a72b04e16a679ae9fe18ad2b38555a3
marietopphem <marietopphem@gmail.com>
2017-05-12 14:57:27 +0200

    la till filer som saknas

---
+++ app/src/main/java/com/example/marietopphem/groupout1/FavoriteSearch.java
@@ -0,0 +1,22 @@
+package com.example.marietopphem.groupout1;
+
+/**
+ * Created by marietopphem on 2017-05-12.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FavoriteSearch extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.t4favorite, container, false);
+        return rootView;
+    }
+}
