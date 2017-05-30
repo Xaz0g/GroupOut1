e17c45f981f96dc79f194d50b03345302f4df3d4
Axel Andersson <lexa.andersson@gmail.com>
2017-05-30 17:06:03 +0200

    Catsearchstuff

--- app/src/main/java/com/example/marietopphem/groupout1/CategorySearch.java
+++ app/src/main/java/com/example/marietopphem/groupout1/CategorySearch.java
@@ -4,6 +4,8 @@ package com.example.marietopphem.groupout1;
  * Created by marietopphem on 2017-05-12.
  */
 
+        import android.content.SharedPreferences;
+        import android.preference.PreferenceManager;
         import android.support.v4.app.Fragment;
         import android.os.Bundle;
         import android.util.Log;
@@ -12,90 +14,128 @@ package com.example.marietopphem.groupout1;
         import android.view.ViewGroup;
         import android.widget.CheckBox;
         import android.widget.EditText;
+        import android.widget.ListView;
         import android.widget.TextView;
 
+        import org.json.JSONArray;
+        import org.json.JSONException;
+
+        import java.util.ArrayList;
         import java.util.concurrent.ExecutionException;
 
+        import handlers.EventActivity;
         import handlers.HttpHandler;
         import handlers.HttpTask;
+        import handlers.PlaceEventAdapter;
+        import models.EveObject;
 
 public class CategorySearch extends Fragment{
+
+    private ArrayList<EveObject> eventList;
+    private PlaceEventAdapter placeEventAdapter;
+    private SharedPreferences sharedPrefs;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.t2category, container, false);
 
+        this.sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext());
+
+        eventList = new ArrayList<>();
+
+        placeEventAdapter = new PlaceEventAdapter(getActivity().getApplicationContext(), eventList);
+        placeEventAdapter.setToken(sharedPrefs.getString("Token", "FAIL"));
+        placeEventAdapter.setUserId(Integer.parseInt(sharedPrefs.getString("userId", "FAIL")));
+
+        ListView lv = (ListView) rootView.findViewById(R.id.eventlistcat);
+        lv.setAdapter(placeEventAdapter);
+
+
+
         return rootView;
     }
     //OBS! denna metod aktiveras inte och kan därför inte kolla om den är korrekt
-    public void searchForCategory(View v){
-        if(v.getId() == R.id.searchButton){
-            EditText s = (EditText) getView().findViewById(R.id.categorySearchTextField);
-            String searchPattern = s.getText().toString();
+    public void searchEvent() {
 
-            CheckBox cardioCheck = (CheckBox) getView().findViewById(R.id.checkCardio);
-            CheckBox mobilityCheck = (CheckBox) getView().findViewById(R.id.checkMobility);
-            CheckBox strengthTrainingCheck = (CheckBox) getView().findViewById(R.id.checkStrengthTraining);
-            CheckBox ballSportsCheck = (CheckBox) getView().findViewById(R.id.checkBallSports);
+        EditText s = (EditText) getView().findViewById(R.id.categorySearchTextField);
+        String searchPattern = s.getText().toString();
+        searchPattern = (searchPattern == null || searchPattern.equals("")) ? "*" : searchPattern;
+        Log.d("CASEA", searchPattern);
 
-            boolean cardio = cardioCheck.isChecked();
-            boolean mobility = mobilityCheck.isChecked();
-            boolean strengthTraining = strengthTrainingCheck.isChecked();
-            boolean ballSports = ballSportsCheck.isChecked();
+        CheckBox cardioCheck = (CheckBox) getView().findViewById(R.id.checkCardio);
+        CheckBox mobilityCheck = (CheckBox) getView().findViewById(R.id.checkMobility);
+        CheckBox strengthTrainingCheck = (CheckBox) getView().findViewById(R.id.checkStrengthTraining);
+        CheckBox ballSportsCheck = (CheckBox) getView().findViewById(R.id.checkBallSports);
 
-            String categoryString = "";
-            int cats = 0;
-            if(cardio)
-            {
-                categoryString += "Kondition";
-                cats++;
+        boolean cardio = cardioCheck.isChecked();
+        boolean mobility = mobilityCheck.isChecked();
+        boolean strengthTraining = strengthTrainingCheck.isChecked();
+        boolean ballSports = ballSportsCheck.isChecked();
+
+        String categoryString = "";
+        int cats = 0;
+        if (cardio) {
+            categoryString += "Kondition";
+            cats++;
+        }
+
+        if (mobility) {
+            if (cardio) {
+                categoryString += "-";
             }
 
-            if(mobility)
-            {
-                if(cardio)
-                {
-                    categoryString += "-";
-                }
+            categoryString += "Rörlighet+och+dans";
+            cats++;
+        }
 
-                categoryString += "Rörlighet+och+dans";
-                cats++;
+        if (strengthTraining) {
+            if (cardio || mobility) {
+                categoryString += "-";
             }
 
-            if(strengthTraining)
-            {
-                if(cardio || mobility)
-                {
-                    categoryString += "-";
-                }
+            categoryString += "Styrketräning";
+            cats++;
+        }
 
-                categoryString += "Styrketräning";
-                cats++;
+        if (ballSports) {
+            if (cardio || mobility || strengthTraining) {
+                categoryString += "-";
             }
 
-            if (ballSports)
-            {
-                if(cardio || mobility || strengthTraining)
-                {
-                    categoryString += "-";
-                }
+            categoryString += "Bollsport";
+            cats++;
+        }
 
-                categoryString += "Bollsport";
-                cats++;
-            }
+        String token = sharedPrefs.getString("Token", "FAIL");
+        String request = (cats > 0 && cats <= 4) ? HttpHandler.eventSearch(searchPattern, categoryString, token) :
+                HttpHandler.eventSearch(searchPattern, "All", token);
 
-            String request = (cats > 0 && cats <= 4) ? HttpHandler.eventSearch(searchPattern, categoryString) :
-                    HttpHandler.eventSearch(searchPattern, "All");
+        Log.d("CASEA", request);
 
-            try {
-                String response = new HttpTask().execute("get", request).get();
+        getEvents(request);
 
-            } catch (InterruptedException e) {
-                Log.e("CATSEA", e.getMessage());
-            } catch (ExecutionException e) {
-                Log.e("CATSEA", e.getMessage());
-            }
+
+    }
+
+    private void getEvents(String request){
+        try {
+            String response = new HttpTask().execute("get", request).get();
+
+            Log.d("CASEA", response);
+
+            ArrayList<EveObject> temp = EventActivity.parseJSONv2(response);
+            eventList.clear();
+            eventList.addAll(temp);
+            placeEventAdapter.notifyDataSetChanged();
+
+        } catch (InterruptedException e) {
+            Log.e("CATSEA", e.getMessage());
+        } catch (ExecutionException e) {
+            Log.e("CATSEA", e.getMessage());
+        } catch (JSONException e) {
+            Log.e("CATSEA", e.getMessage());
         }
     }
+
 }
--- app/src/main/java/handlers/EventActivity.java
+++ app/src/main/java/handlers/EventActivity.java
@@ -48,4 +48,40 @@ public class EventActivity {
 
         return eveList;
     }
+
+    public static ArrayList<EveObject> parseJSONv2 (String json) throws JSONException{
+        JSONArray events = new JSONArray(json);
+        int n = events.length();
+
+        ArrayList<EveObject> eveList = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+
+            JSONObject event = events.getJSONObject(i);
+
+            int id = event.getInt("id");
+            int leaderId = event.getInt("leaderId");
+            int minCapacity = event.getInt("minCapacity");
+            int maxCapacity = event.getInt("maxCapacity");
+            int registration = event.getInt("registration");
+            int difficulty = event.getInt("difficulty");
+            String name = event.getString("name");
+            String category = event.getString("category");
+            String description = event.getString("description");
+            String placeId = event.getString("placeId");
+            String eventDate = event.getString("eventDate");
+            String startTime = event.getString("startTime");
+            String endTime = event.getString("endTime");
+            Boolean visible = event.getBoolean("visible");
+            Boolean participating = event.getBoolean("participating");
+            String placeName = event.getString("placeName");
+
+
+            EveObject eve = new EveObject(name, category, description, placeId, eventDate, startTime, endTime, visible, id, leaderId, minCapacity, maxCapacity, registration, difficulty, participating, placeName);
+            eveList.add(eve);
+
+        }
+
+        return eveList;
+    }
 }
